ab5b78bb2d757f4cfacdf620e6418152b7ebbf52
Blake Boswell <j.blake.boswell@gmail.com>
2015-12-07 02:02:18 -0500

    moved date filter to api call

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 from flask import Markup
 import requests
+import datetime
 
 from bokeh.plotting import figure
 from bokeh.resources import CDN
@@ -10,9 +11,17 @@ import numpy as np
 import pandas as pd
 
 def create_plot(symbl, name):
-	'''
-	build bokeh plot, return (script, div)
-	'''
+	"""
+	method for creating bokeh plot
+
+	Args:
+		symbl: valid stock ticker symbol
+		name: company name, used for plot title
+
+	Returns:
+		(script, div) bokeh script and div markup
+
+	"""
 	plot = figure(title=name, tools='wheel_zoom, pan', 
 				  responsive=True, plot_width=1000,
 				  plot_height=500, x_axis_type='datetime')
@@ -20,24 +29,33 @@ def create_plot(symbl, name):
 	plot.line(df['date'], df['close'], legend='Closing Price')
 	plot.legend.orientation = 'top_left'
 	plot.legend.background_fill_alpha = 0.0
-	plot.yaxis[0].formatter = NumeralTickFormatter(format="$0.00")
+	plot.yaxis[0].formatter = NumeralTickFormatter(format='$0.00')
 	script, div = components(plot, CDN)
 
 	return Markup(script), Markup(div)
 
 
 def get_data(symbl):
-	'''
-	get stock data from quandl, return date and {date, close}
-	'''
-	api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=t-drH_WSpLdRenh1o86E' % symbl
+  	"""
+    method for requesting data from quandl api
+        
+    Args:
+        symbl: valid stock ticker symbol
+            
+    Returns:
+        {date, closing price}
+               
+    """
+	end_date = datetime.datetime.now()
+	start_date = end_date + datetime.timedelta(-30)
+	api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=t-drH_WSpLdRenh1o86E&start_date=%s&end_date=%s' \
+				% (symbl, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
 	session = requests.Session()
 	session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-	raw_json = session.get(api_url).json()
-	# raw_json['data']: [date, open, high, low, close, volume, ex-dividend, splitratio, adjopen, adjhigh, adjlow, adjclose, adjvolume]
+	raw_json = session.get(api_url).json()['dataset']
 	df = pd.DataFrame({
-			'date' : np.array([x[0] for x in raw_json['data']]),
+			'date' : [x[0] for x in raw_json['data']],
 			'close' : np.array([x[4] for x in raw_json['data']]),
 		})
 	df['date'] = pd.to_datetime(df['date'])
-	return df.head(30)
+	return df
