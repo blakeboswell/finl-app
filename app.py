6a173f160230a9cd80054c7fc4c373f5b8d23077
Blake Boswell <j.blake.boswell@gmail.com>
2015-12-06 23:57:12 -0500

    added company name, legend, and axis formatting

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ tickers = pd.read_csv('https://s3.amazonaws.com/quandl-static-content/Ticker+CSV
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+	
 	'''
-	main page of the app
-	POST: check that symbol is valid and create plot
+	POST: check that symbol is valid, create plot, reset search bar
 	GET: show search bar with input message
 	'''
+
 	if request.method == 'POST' and 'symbol' in request.form:
 		symbl = request.form['symbol'].upper()
 		if 'WIKI/'+ symbl in tickers['quandl code'].values:
 			try:
-				script, div = create_plot(symbl)
+				name = tickers.loc[tickers['quandl code'] == 'WIKI/' + symbl, 'name'].values[0]
+				script, div = create_plot(symbl, name)
 				return render_template('index.html', place_holder= "Input Stock Symbol...", 
 							plot_script=script, plot_div=div)
 			except:
--- plot.py
+++ plot.py
@@ -4,19 +4,23 @@ import requests
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import components
+from bokeh.models import NumeralTickFormatter
 
 import numpy as np
 import pandas as pd
 
-def create_plot(symbl):
+def create_plot(symbl, name):
 	'''
 	build bokeh plot, return (script, div)
 	'''
-	plot = figure(title=symbl + ' Closing Price', tools='wheel_zoom, pan', 
+	plot = figure(title=name, tools='wheel_zoom, pan', 
 				  responsive=True, plot_width=1000,
 				  plot_height=500, x_axis_type='datetime')
 	df = get_data(symbl)
-	plot.line(df['date'], df['close'])
+	plot.line(df['date'], df['close'], legend='Closing Price')
+	plot.legend.orientation = 'top_left'
+	plot.legend.background_fill_alpha = 0.0
+	plot.yaxis[0].formatter = NumeralTickFormatter(format="$0.00")
 	script, div = components(plot, CDN)
 
 	return Markup(script), Markup(div)
@@ -26,7 +30,7 @@ def get_data(symbl):
 	'''
 	get stock data from quandl, return date and {date, close}
 	'''
-	api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % symbl
+	api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=t-drH_WSpLdRenh1o86E' % symbl
 	session = requests.Session()
 	session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
 	raw_json = session.get(api_url).json()
@@ -35,4 +39,5 @@ def get_data(symbl):
 			'date' : np.array([x[0] for x in raw_json['data']], dtype=np.datetime64),
 			'close' : np.array([x[4] for x in raw_json['data']]),
 		})
+	
 	return df
